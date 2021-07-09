ca5c14e933e27c488c065631d0ff6af6b6f9ca10
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-07-09 16:01:50 -0600

    added environment map

--- frontend/src/ui/AnimationScene.js
+++ frontend/src/ui/AnimationScene.js
@@ -1,23 +1,25 @@
 import React, { Suspense } from "react";
 
 
-
-
 import "./animation.css";
 import {Canvas} from "@react-three/fiber";
-import {OrbitControls} from "@react-three/drei";
+import {OrbitControls, Environment} from "@react-three/drei";
 import Barret from "../Barret";
+// import "../../public/simons_town_rocks_1k.hdr"
+import {presetsObj} from "@react-three/drei/helpers/environment-assets";
 
 export default function AnimationScene() {
     return (
         <>
         <Canvas shadows camera={{ position: [1, 2, 3], fov: 60 }}>
+
+
             <OrbitControls />
             {/*<ambientLight intensity={1} />*/}
             <directionalLight
                 castShadow
                 position={[0, 10, 0]}
-                intensity={1.5}
+                intensity={1}
                 shadow-mapSize-width={1024}
                 shadow-mapSize-height={1024}
                 shadow-camera-far={50}
@@ -26,16 +28,17 @@ export default function AnimationScene() {
                 shadow-camera-top={10}
                 shadow-camera-bottom={-10}
             />
-            <pointLight position={[-10, 0, -20]} intensity={0.5}/>
-            <pointLight position={[0, 0, 0]} intensity={1.5}/>
+            {/*<pointLight position={[-10, 0, -20]} intensity={0.5}/>*/}
+            {/*<pointLight position={[0, 0, 0]} intensity={1.5}/>*/}
             <Suspense fallback={null}>
-                <Barret />
+                <Environment files={'simons_town_rocks_4k.hdr'} background={true}/>
+                <Barret position={[0,0,0]} />
             </Suspense>
 
             <group>
                 <mesh
                 rotation={[-Math.PI / 2, 0, 0]}
-                position={[0, -3, 0]}
+                position={[0, 0, 0]}
                 receiveShadow>
                 <planeBufferGeometry attach='geometry' args={[100, 100]}/>
                 {/*<shadowMaterial attach='material' opacity={0.3} />*/}
