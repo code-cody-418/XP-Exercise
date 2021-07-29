625f7647b9eec701d61956e879ce55a14fdd4760
Brody-code-designer <80767371+Brody-code-designer@users.noreply.github.com>
2021-07-29 07:03:10 -0600

    finished logic for action to text

--- frontend/src/ui/DisplayAction.js
+++ frontend/src/ui/DisplayAction.js
@@ -33,7 +33,79 @@ export const DisplayAction = ({name, narutoAction, korraAction, gokuAction, kaka
                 return setTextAnimation(moves.capitalCoolDown)
             }
         } else if (name === names.korra) {
-
+            if (korraAction === moves.idle) {
+                return setTextAnimation(moves.capitalIdle)
+            } else if (korraAction === moves.armStretch) {
+                return setTextAnimation(moves.capitalArmStretch)
+            }  else if (korraAction === moves.touchToes) {
+                return setTextAnimation(moves.capitalTouchToes)
+            }  else if (korraAction === moves.burpee) {
+                return setTextAnimation(moves.capitalBurpee)
+            }  else if (korraAction === moves.crossJumps) {
+                return setTextAnimation(moves.capitalCrossJumps)
+            }  else if (korraAction === moves.crossRotation) {
+                return setTextAnimation(moves.capitalCrossRotation)
+            }  else if (korraAction === moves.pushUp) {
+                return setTextAnimation(moves.capitalPushUp)
+            }  else if (korraAction === moves.sitUps) {
+                return setTextAnimation(moves.capitalSitUps)
+            }  else if (korraAction === moves.bicepCurl) {
+                return setTextAnimation(moves.capitalBicepCurl)
+            }  else if (korraAction === moves.coolDown) {
+                return setTextAnimation(moves.capitalCoolDown)
+            }
+        } else if (name === names.goku) {
+            if (gokuAction === moves.idle) {
+                return setTextAnimation(moves.capitalIdle)
+            } else if (gokuAction === moves.touchToes) {
+                setTextAnimation(moves.capitalTouchToes)
+            }  else if (gokuAction === moves.armStretch) {
+                setTextAnimation(moves.capitalArmStretch)
+            }  else if (gokuAction === moves.jumpingJack) {
+                setTextAnimation(moves.capitalJumpingJack)
+            }  else if (gokuAction === moves.jab) {
+                setTextAnimation(moves.capitalJab)
+            }  else if (gokuAction === moves.hook) {
+                setTextAnimation(moves.capitalHook)
+            }  else if (gokuAction === moves.punchCombo) {
+                setTextAnimation(moves.capitalPunchCombo)
+            }  else if (gokuAction === moves.bicepCurl) {
+                setTextAnimation(moves.capitalBicepCurl)
+            }  else if (gokuAction === moves.plank) {
+                setTextAnimation(moves.capitalPlank)
+            }  else if (gokuAction === moves.sitUps) {
+                setTextAnimation(moves.capitalSitUps)
+            }  else if (gokuAction === moves.pushUp) {
+                setTextAnimation(moves.capitalPushUp)
+            }  else if (gokuAction === moves.coolDown) {
+                setTextAnimation(moves.capitalCoolDown)
+            }
+        } else if (name === names.naruto) {
+            if (narutoAction === moves.idle) {
+                return setTextAnimation(moves.capitalIdle)
+            } else if (narutoAction === moves.armStretch) {
+                return setTextAnimation(moves.capitalArmStretch)
+            } else if (narutoAction === moves.touchToes) {
+                return setTextAnimation(moves.capitalTouchToes)
+            } else if (narutoAction === moves.upRock) {
+                return setTextAnimation(moves.capitalUpRock)
+            } else if (narutoAction === moves.footwork) {
+                return setTextAnimation(moves.capitalFootwork)
+            } else if (narutoAction === moves.freeze) {
+                return setTextAnimation(moves.capitalFreeze)
+            } else if (narutoAction === moves.jumpingJack) {
+                return setTextAnimation(moves.capitalJumpingJack)
+            } else if (narutoAction === moves.bikeCrunch) {
+                return setTextAnimation(moves.capitalBikeCrunch)
+            } else if (narutoAction === moves.sitUps) {
+                return setTextAnimation(moves.capitalSitUps)
+            } else if (narutoAction === moves.pushUp) {
+                return setTextAnimation(moves.capitalPushUp)
+            } else if (narutoAction === moves.bicepCurl) {
+                return setTextAnimation(moves.capitalBicepCurl)
+            } else if (narutoAction === moves.coolDown) {
+                return setTextAnimation(moves.capitalCoolDown)
+            }
         }
     }, [kakashiAction, korraAction, narutoAction, gokuAction])
 
